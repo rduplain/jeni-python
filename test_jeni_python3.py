222935ffc347f9787f08b50cccb1981151db5cec
Ron DuPlain <ron.duplain@gmail.com>
2015-06-10 18:18:39 -0400

    Test for missing __annotations__ in Python 3.

--- test_jeni_python3.py
+++ test_jeni_python3.py
@@ -12,6 +12,24 @@ class Python3AnnotationTestCase(unittest.TestCase):
         jeni.annotate(fn)
         self.assertTrue(jeni.annotate.has_annotations(fn))
 
+    def test_annotate_without_dunder_annotations(self):
+        # Unclear when this would come up; testing it given Python 2 support.
+        class NoDunderAnnotations(object):
+            def __getattr__(self, name):
+                if name == '__annotations__':
+                    raise AttributeError()
+                return super().__getattr__(name)
+
+            def __call__(self):
+                "unused"
+
+        fn = NoDunderAnnotations()
+        self.assertTrue(hasattr(fn, '__call__'))
+        self.assertFalse(hasattr(fn, '__annotations__'))
+        self.assertFalse(hasattr(fn, 'fake')) # coverage
+        with self.assertRaises(AttributeError):
+            jeni.annotate(fn)
+
 
 @jeni.annotate
 def annotated_function(hello: 'hello:thing', eggs: 'eggs'):
